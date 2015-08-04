56718f47c81eaaf515a310acf0df5e1f1ca388d9
tunnckoCore <mameto_100@mail.bg>
2015-08-04 12:32:26 +0300

    add tests

--- test.js
+++ test.js
@@ -2,6 +2,24 @@
 
 var fs = require('fs')
 var jscss = require('./index')
-var input = fs.readFileSync('./fixture.css', 'utf8')
+var test = require('assertit')
 
-console.log(jscss(input))
+var input = fs.readFileSync('./fixture.css', 'utf8')
+var expected =  fs.readFileSync('./expected.css', 'utf8')
+
+test('jscss', function () {
+  test('should compile given string to CSS (sync api)', function (done) {
+    var actual = jscss(input)
+    test.equal(actual, expected, 'should be equal')
+    done()
+
+  })
+  test('should compile string to CSS (callback api)', function (done) {
+    var input = fs.readFileSync('./fixture.css', 'utf8')
+    jscss(input, function (err, actual) {
+      test.ifError(err)
+      test.equal(actual, expected, 'should be equal')
+      done()
+    })
+  })
+})
